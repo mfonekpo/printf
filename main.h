fcaf531ee5a729ce658e7b81bacb82e3450bf971
08168005790 <amehene2017@gmail.com>
2023-10-14 03:05:04 -0700

    task 3

--- main.h
+++ main.h
@@ -9,5 +9,7 @@ int print_string(char *s);
 int _printf2(const char *format, ...)
 int _printf3(const char *format, ...)
 int print_binary(unsigned int num)
+int _printf4(const char *format, ...);
+
 #endif /* MAIN_H */
 
