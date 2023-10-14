edb63578316b3ca7208bac4aec198e78d59283d3
mfonekpo <mfonekpo34@gmail.com>
2023-10-14 11:20:10 +0100

    task_4

--- main.h
+++ main.h
@@ -6,10 +6,10 @@
 int _printf(const char *format, ...);
 int print_char(char c);
 int print_string(char *s);
-int _printf2(const char *format, ...)
-int _printf3(const char *format, ...)
-int print_binary(unsigned int num)
+int _printf2(const char *format, ...);
+int _printf3(const char *format, ...);
+int print_binary(unsigned int num);
 int _printf4(const char *format, ...);
-
+void buffer_to_stdout(void);
 #endif /* MAIN_H */
 
